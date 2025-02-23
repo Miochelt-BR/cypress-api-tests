74ac394e42ad25fd423dbbad59db5d1df11f4872
thiag <thiago.arica@outlook.com>
2025-02-22 23:08:48 -0300

    feat: atualizar

--- cypress/e2e/buscarAgendamentos.cy.js
+++ cypress/e2e/buscarAgendamentos.cy.js
@@ -1,13 +1,16 @@
+/// <reference types="cypress" />
+
 describe("Buscar agendamentos", () => {
   
   it("Deve buscar agendamento com sucesso", () => {
     cy.request({
       method: "GET",
-      url: "https://restful-booker.herokuapp.com/booking/664"
-    }).then((response) => {
-      expect(response.status).to.equal(200); 
-      
-      
+      url: "https://restful-booker.herokuapp.com/booking/764",
+     failOnStatusCode: false 
+    }).as("getResultStatus");
+
+    cy.get("@getResultStatus").then((response) => {
+      console.log(response);
     });
   });
 
