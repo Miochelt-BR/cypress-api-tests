514ac7bad7aa72171ef15df8c0e6328b1c632960
thiag <thiago.arica@outlook.com>
2025-03-10 21:37:32 -0300

    refactor: melhorar fluxo de criação e busca de agendamentos

--- cypress/e2e/buscarAgendamentos.cy.js
+++ cypress/e2e/buscarAgendamentos.cy.js
@@ -1,22 +1,55 @@
-/// <reference types="cypress" />
+/// /// <reference types="cypress" />
 
 describe("Buscar agendamentos", () => {
   
-  it("Deve buscar agendamento com sucesso", () => {
-    const bookingId = '33'; 
+  it("Deve criar e buscar agendamento com sucesso", () => {
+    cy.request({
+      method: "POST",
+      url: "https://restful-booker.herokuapp.com/booking",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: {
+        "firstname": "Thiago",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": true,
+        "bookingdates": {
+          "checkin": "2018-01-01",
+          "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+      }
+    }).then((postResponse) => {
+      expect(postResponse.status).to.equal(200);
+      expect(postResponse.body).to.have.property("bookingid");
+
+      const bookingId = postResponse.body.bookingid; // Captura o ID criado
+
+      
+      cy.request({
+        method: "GET",
+        url: `https://restful-booker.herokuapp.com/booking/${bookingId}`,
+        failOnStatusCode: false
+      }).then((getResponse) => {
+        expect(getResponse.status).to.equal(200);
+        expect(getResponse.body).to.be.an("object");
+        expect(getResponse.body.firstname).to.equal("Thiago");
+        expect(getResponse.body.lastname).to.equal("Brown");
+      });
+    });
+  });
+
+  it("Deve buscar agendamento sem sucesso", () => {
+    const invalidBookingId = '999999'; 
 
     cy.request({
       method: "GET",
-      url: `https://restful-booker.herokuapp.com/booking/${bookingId}`,
+      url: `https://restful-booker.herokuapp.com/booking/${invalidBookingId}`,
       failOnStatusCode: false
     }).then((response) => {
-      expect(response.status).to.equal(200);
-
-
-      expect(response.body).to.be.an("object");
-     
-
-      
+      expect(response.status).to.equal(404);
+      expect(response.body).to.equal("Not Found"); 
     });
   });
 
